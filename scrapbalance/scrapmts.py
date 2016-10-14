dcc25e525f0de4fbb0a3cedf219e1da5380546a3
Igor Kozyrenko <igor@ikseek.com>
2016-10-14 14:50:28 +0300

    Add additional mts phone prefixes

--- scrapbalance/scrapmts.py
+++ scrapbalance/scrapmts.py
@@ -5,7 +5,7 @@ BALANCE_REGEX = regex(r'Ваш текущий баланс: ([0-9,]+) грн')
 
 
 class MTSHelper(MobileScrapper):
-    PHONE_FORMAT_REGEX = regex(r"\+380((99)\d{7})$")
+    PHONE_FORMAT_REGEX = regex(r"\+380((50|95|99|66)\d{7})$")
 
     @staticmethod
     def login(phone, password):
